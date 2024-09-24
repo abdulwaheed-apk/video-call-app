9e7792f0e036499ed9c52860ddc8bf7c4668d285
Abdul Waheed <ab.waheed1722@gmail.com>
2024-09-24 22:58:16 +0300

    make call, other peer receive call

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useSocket } from '../providers/Socket'
 import ReactPlayer from 'react-player'
+import peer from '../service/peer'
 
 export default function RoomPage() {
     const [remoteSocketId, setRemoteSocketId] = useState(null)
@@ -12,20 +13,29 @@ export default function RoomPage() {
         setRemoteSocketId(data.id)
     }, [])
 
+    const handleInComingCall = useCallback(({ from, offer }) => {
+        console.log(`incoming call ${from} , ${offer}`)
+    }, [])
+
     const handleCall = useCallback(async () => {
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: true,
             video: true,
         })
+        const offer = await peer.getOffer()
+        socket.emit('user:call', { to: remoteSocketId, offer })
         setMyStream(stream)
-    }, [])
+    }, [remoteSocketId, socket])
 
     useEffect(() => {
         socket.on('user:joined', handleUserJoined)
+        socket.on('incoming:call', handleInComingCall)
+
         return () => {
             socket.off('user:joined', handleUserJoined)
+            socket.off('incoming:call', handleInComingCall)
         }
-    }, [socket, handleUserJoined])
+    }, [socket, handleUserJoined, handleInComingCall])
 
     return (
         <>
@@ -33,13 +43,16 @@ export default function RoomPage() {
             <h4>{remoteSocketId ? 'Connected' : 'No one in the room'}</h4>
             {remoteSocketId && <button onClick={handleCall}>Call</button>}
             {myStream && (
-                <ReactPlayer
-                    muted
-                    playing
-                    width={'300px'}
-                    height={'200px'}
-                    url={myStream}
-                />
+                <>
+                    <h4>My Stream</h4>
+                    <ReactPlayer
+                        muted
+                        playing
+                        width={'300px'}
+                        height={'200px'}
+                        url={myStream}
+                    />
+                </>
             )}
         </>
     )
